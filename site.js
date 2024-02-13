f3ada135aaea42cd2ce00f39e1134d1d8b0f6f01
katasavy <katasavy@katasavy-VM.myguest.virtualbox.org>
2024-02-13 00:12:31 -0500

    Adding initial index file

---
+++ site.js
@@ -0,0 +1,54 @@
+
+var shown=false;
+
+function showHideEmail(){
+  if(shown){
+    document.getElementById('email').innerHTML="Show my email";
+    shown=false;
+  }
+  else{
+    var myEmail="<a href='mailto:katasavy"+"@"+"mail.uc.edu'>katasavy"+
+    "@"+"mail.uc.edu";
+    document.getElementById('email').innerHTML=myEmail;
+    shown=true;
+  }
+}
+//digital clock
+function displayTime(){
+      document.getElementById('digital-clock').innerHTML="Current time: "+ new Date();
+    }
+setInterval(displayTime,500);
+
+function jokeAPIHit(){
+  $.get("https://v2.jokeapi.dev/joke/Programming?type=single",
+      function(result){
+        if(result==null || result.joke==null || result.joke==""){
+          alert("Did not receive output from joke API");
+        }       
+        else{
+          $("#response").html("<b>A programming joke of the day: </b>"+encodeInput(result.joke));
+        }
+      }
+    );
+}
+setInterval(jokeAPIHit,10000);
+
+async function guessAge(){
+      const response=await fetch("https://xkcd.com/info.0.json");
+      const result=await response.json();
+      
+      if(result==null || result.img==null || result.img==""){
+        alert("Did not receive output from agify API");
+        }       
+      else{
+        $("#image").html(result.img);
+        }
+        
+    }
+
+function encodeInput(input){
+      const encodedData=document.createElement('div');
+      encodedData.innerText=input;
+      return encodedData.innerHTML;
+}
+
